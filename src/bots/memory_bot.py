da8ed0a5d12d393eddb0e93a72659036eea4a265
Oleh <34406675+justOleh@users.noreply.github.com>
2024-08-10 03:16:26 +0300

    Feature/memory bot (#6)
    * Add some rough implementation
    
    * Merge feature/test_screenshot_time

--- src/bots/memory_bot.py
+++ src/bots/memory_bot.py
@@ -5,9 +5,29 @@ from src.bots.bot import Bot
 
 class MemoryBot(Bot):
     def __init__(self):
-        self.critical_distance = 40
-        self.angle_threshold = 10
+        self.critical_distance = 50
+
+        self.action_count_rotate = 3
+        self.action_count_pass = 0
+        self.is_start_count = False
 
     def action(self, state):
-        # TODO: implement logic here
-        pass
+        if self.is_start_count:
+            self.action_count_pass += 1
+
+        cannonball_centers_normalised = state["cannonball_centers"]
+        boat_center_normalised = state["boat_center"]
+
+        # Actions based on distances
+        distances = np.linalg.norm(cannonball_centers_normalised-boat_center_normalised, ord=1, axis=1)
+
+        if self.action_count_pass >= self.action_count_rotate:
+            print(self.action_count_pass)
+            self.is_start_count = False
+            self.action_count_pass = 0
+            print("change dir oposite")
+            return "change_direction"
+
+        if any(dist <= self.critical_distance for dist in distances):
+            self.is_start_count = True
+            return "change_direction"
